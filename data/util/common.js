fa813aa411ac5d55c1448c34098f7d8f41364599
Michael Neuweiler <michael_neuweiler@yahoo.com>
2020-03-30 01:09:45 +0200

    replace complex value updating with simple jquery

--- data/util/common.js
+++ data/util/common.js
@@ -1,51 +1,7 @@
-var nodecache = new Array();
-
-function getCache(name) {
-	var target = nodecache[name];
-	if (!target) {
-		target = document.getElementById(name);
-		if (!target) { // id not found, try to find by name
-			var namedElements = document.getElementsByName(name);
-			if (namedElements && namedElements.length) {
-				target = namedElements[0];
-			}
-		}
-		nodecache[name]=target;
-	}
-	return target;
-}
-
 function setNodeValue(name, value) {
-	// a normal div/span/input to update
-	target = getCache(name);
-	if (target) { // found an element, update according to its type
-		if (target.nodeName.toUpperCase() == 'DIV' || target.nodeName.toUpperCase() == 'SPAN') {
-			target.innerHTML = value;
-		} else if (target.nodeName.toUpperCase() == 'INPUT') {
-			if (target.type && (target.type.toUpperCase() == 'CHECKBOX' || target.type.toUpperCase() == 'RADIO')) {
-				target.checked = value;
-			} else {
-				target.value = value;
-				// find corresponding slider element
-				var slider = document.getElementById(name + "Level");
-				if (slider) {
-					slider.value = value;
-				}
-			}
-		} else if (target.nodeName.toUpperCase() == 'SELECT') {
-			selectItemByValue(target, value);
-		}
-	}
-}
-
-
-// scan through the options of a select input field and activate the one with
-// the given value
-function selectItemByValue(node, value) {
-	for (var i = 0; i < node.options.length; i++) {
-		if (node.options[i].value == value) {
-			node.selectedIndex = i;
-			break;
-		}
-	}
+	var targets = $("[name='" + name + "'],[id='" + name + "']")
+	targets.filter("div,span").html(value);
+	targets.filter("input[type='checkbox']").prop("checked", value);
+	targets.filter("select,input[type!='checkbox']").val(value);
+	$("#" + name + "Level").val(value);
 }
