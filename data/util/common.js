be399799d3ebd58b6fdb6bde446cbfaa81e4e092
Michael Neuweiler <michael_neuweiler@yahoo.com>
2020-08-07 11:39:28 +0200

    minor gui fixes

--- data/util/common.js
+++ data/util/common.js
@@ -1,4 +1,7 @@
 function setNodeValue(name, value) {
+	if (Number.isInteger(value) && (name.startsWith('dc') || name.startsWith('charger'))) {
+		value = value.toFixed(1);
+	}
 	var targets = $("[name='" + name + "'],[id='" + name + "']")
 	targets.filter("div,span").html(value);
 	targets.filter("input[type='checkbox']").prop("checked", value);
